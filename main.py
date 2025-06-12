e705a13c46da776b8247272f4fee14baf2689cbc
Andrew Lu <andrewlu.2004.2020@gmail.com>
2025-06-12 15:30:24 -0400

    basic sentiment extraction successful

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+analyzer = SentimentIntensityAnalyzer()
+sentiments = []
+headlines = [
+  "Los Angeles ICE detention protests",
+  "Elon Musk deletes his X posts attacking Donald Trump",
+  "Apple's WWDC reveals a new 'Liquid Glass' aesthetic",
+  "China and US reach a trade deal to Trump's liking"
+]
+for headline in headlines:
+  scores = analyzer.polarity_scores(headline)
+  sentiments.append({
+    'Headline': headline,
+    'Negative': scores['neg'],
+    'Neutral': scores['neu'],
+    'Positive': scores['pos'],
+    'Compound': scores['compound']
+  })
+
+for sentiment in sentiments:
+  plt.figure(figsize=(8, 5))
+  sns.barplot(x=['Negative', 'Neutral', 'Positive'], y=[sentiment['Negative'], sentiment['Neutral'], sentiment['Positive']])
+  plt.title(f'Sentiment Scores for {sentiment['Headline']}')
+  plt.ylabel('Score')
+  plt.savefig(f'output/sentiment_{sentiment['Headline'].replace(" ", "_")}.png')
+  plt.show()
